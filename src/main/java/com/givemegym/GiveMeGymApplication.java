694bb7a72259b36709749b2d363c1ec836f13835
Calara <chloe830715@gmail.com>
2023-06-07 21:37:36 +0800

    no message

---
+++ src/main/java/com/givemegym/GiveMeGymApplication.java
@@ -0,0 +1,13 @@
+package com.givemegym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveMeGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveMeGymApplication.class, args);
+    }
+
+}
