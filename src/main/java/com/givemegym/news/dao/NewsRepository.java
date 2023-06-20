7b08d5bb32d077afd18a5a8e12c4a0ab20aeff1e
ChiaJungHsu <goody2shes13@gmail.com>
2023-06-20 10:19:24 +0800

    update News,CourseShowoff commit

--- src/main/java/com/givemegym/news/dao/NewsRepository.java
+++ src/main/java/com/givemegym/news/dao/NewsRepository.java
@@ -3,10 +3,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
-import com.givemegym.news.vo.NewsVo;
+import com.givemegym.news.vo.News;
 
 @Repository
-public interface NewsDao extends JpaRepository<NewsVo, Integer>{
+public interface NewsRepository extends JpaRepository<News, Integer>{
 
 
 }
